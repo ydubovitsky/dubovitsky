e3758cc1e6da58999a93160b2d1acd6d8e2169fa
y-dubovitsky <y.dubovitsky@gmail.com>
2018-02-12 19:33:51 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
-
+/**
+* Описание класса Calculate.
+* В нем есть один статический метод main,
+* который выводит в консоль строку Hello Java World.
+* @author - Dubovitsky.
+* @version - 1.0.
+*/
 public class Calculate {
+	/**
+	* Статический метод main который принимает в качестве аргумента
+	* массив строк из командой строки.
+	* @param args - args.
+	* @return void.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello Java World");
 	}
