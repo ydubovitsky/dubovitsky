aca00c8735ad759749b070fc187f98e6172b4441
y-dubovitsky <y.dubovitsky@gmail.com>
2018-02-13 08:40:30 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -16,4 +16,12 @@ public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World");
 	}
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for class calculate test.
+*
+* @author Dubovitsky (y.dubovitsky@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
