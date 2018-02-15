6d4d77f862cc868e49d68d5fd4860b7d81dfee30
y-dubovitsky <y.dubovitsky@gmail.com>
2018-02-15 18:35:36 +0300

    3.4. Максимум из трех чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -7,4 +7,16 @@ public class Max {
 		max = first > second ? first : second;
 		return max;
 	}
+	
+	/*
+	* 1. Метод должен возвращать максимум из трех чисел
+	* 2. В этом методе надо использовать только другой метод
+	* 3. В этом методе не должно быть if или тернарного оператора.
+	*/
+	
+	public int max(int first, int second, int third) {
+		int maxOne = max(first, second);
+		int maxTwo = max(maxOne, third);
+		return maxTwo;
+	}
 }
