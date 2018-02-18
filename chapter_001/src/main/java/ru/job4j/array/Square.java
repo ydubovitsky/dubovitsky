cb510ab9d447966f941a3dd3dd061d3b9d51b492
y-dubovitsky <y.dubovitsky@gmail.com>
2018-02-18 22:26:00 +0300

    5.0. Заполнить массив степенями чисел. Сделал правки.

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -4,7 +4,9 @@ public class Square {
 	public int[] calculate(int bound) {
      int[] rst = new int[bound];
      // заполнить массив через цикл элементами от 1 до bound возведенные в квадрат
-		for(int i = 1; i < bound; i++) rst[i] = i * i;
-    return rsl;
+		for(int i = 0; i < bound; i++) {
+			rst[i] = (int) Math.pow(i, 2);
+		}
+    return rst;
 	}
 }
--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class SquareTest {
 	
@@ -15,8 +16,8 @@ public class SquareTest {
     @Test
     public void calculateTest() {
         Square sq = new Square();
-        int[] result = sq.calculate(3);
-        int[] expected = {1, 4, 9}
+        int[] result = sq.calculate(5);
+        int[] expected = {0, 1, 4, 9, 16};
         assertThat(result, is(expected));
     }
 }
