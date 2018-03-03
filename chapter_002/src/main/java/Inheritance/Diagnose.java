b8052c4dcffc78318844a389153c061671e7e145
y-dubovitsky <y.dubovitsky@gmail.com>
2018-03-03 22:11:52 +0300

    1. Реализация профессий в коде. Написал тест и поправил опечатки в классах.

---
+++ chapter_002/src/main/java/Inheritance/Diagnose.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Diagnose {
+	private String name;
+	
+	public String getDiagnose() {
+		return this.name;
+	}
+
+	public void setDiagnose(String name) {
+		this.name = name;
+	}
+}
