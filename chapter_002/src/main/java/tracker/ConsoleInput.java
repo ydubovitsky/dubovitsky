725a2a8c5837941202e89dc3edbcb668d1782d91
y.dubovitsky <y.dubovitsky@gmail.com>
2018-08-07 11:59:07 +0300

    Рефакторинг - Шаблон Декоратор для валидатора. [#34117] 1. Сделал рефакторинг. 2. Написал плохой тест.

--- chapter_002/src/main/java/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/tracker/ConsoleInput.java
@@ -1,16 +1,33 @@
 package tracker;
 
-
 import java.util.Scanner;
 
+/**
+ * Класс, отвечающий за ввод данных с клавиатуры.
+ */
 public class ConsoleInput implements Input {
+
+    /**
+     *
+     */
     Scanner scanner = new Scanner(System.in);
 
+    /**
+     *
+     * @param question
+     * @return
+     */
     public String ask(String question) {
         System.out.println(question);
         return scanner.nextLine();
     }
 
+    /**
+     *
+     * @param question
+     * @param range
+     * @return
+     */
     public int ask(String question, int range) {
         int key = -1;
         do {
--- chapter_002/src/main/java/tracker/ValidateInput.java
+++ chapter_002/src/main/java/tracker/ValidateInput.java
@@ -1,7 +1,40 @@
 package tracker;
 
-public class ValidateInput extends ConsoleInput {
+/**
+ * Класс, отвечающий за валидацию вводимых данных.
+ */
+public class ValidateInput implements Input {
 
+    /**
+     * Интерфейс, отвечающий за ввод данных.
+     */
+    private final Input input;
+
+    /**
+     * Конструктор принимающий любой!!! объек, реализующий интерфейс Input;
+     * @param input
+     */
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
+
+    /**
+     * Метод вызывает метод ask() у объекта класса реализующего интерфейс Input
+     * и переданный в качестве параметра в конструктор ValidateInput.
+     * @param question
+     * @return
+     */
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
+    /**
+     * Метод проверяет валидность вводимых пользователем данных.
+     * @param question
+     * @param range
+     * @return
+     */
     public int ask(String question, int range) {
         int key = -1;
         do {
