2c3aa6fce243063e74f5470aca49a7e34d3d4d0e
y.dubovitsky <y.dubovitsky@gmail.com>
2018-08-09 14:36:54 +0300

    Очередь с приоритетом на LinkedList [#41670] 1. Метод добавляет задачу в начало связного списка.После добавления, метод сортирует задачи по приоритету.

--- chapter_003/src/main/java/priority/queue/PriorityQueue.java
+++ chapter_003/src/main/java/priority/queue/PriorityQueue.java
@@ -1,22 +1,28 @@
 package priority.queue;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     /**
-     * Метод принимает задачу и ее приоритет.
-     * Можно словить IndexOutOfBoundsException,
-     * если указанный индекс окажется отрицательным или большим текущего значения size.
+     * Метод добавляет задачу в начало связного списка.
+     * После добавления, метод сортирует задачи по приоритету.
      * @param task
      */
     public void put(Task task) {
-        //TODO добавить вставку в связанный список.
-        while (task.getPriority() + 1 > this.tasks.size()) {
-                this.tasks.add(new Task("Нет задач", 0));
+    //TODO добавить вставку в связанный список.
+    tasks.addFirst(task);
+        for (int j = 0; j < tasks.size(); j++) {
+            for (int i = 0; i < tasks.size() - 1; i++) {
+                if (tasks.get(i).getPriority() > tasks.get(i + 1).getPriority()) {
+                    Task temp = tasks.get(i);
+                    tasks.set(i, tasks.get(i + 1));
+                    tasks.set(i + 1, temp);
+                }
+            }
         }
-        this.tasks.set(task.getPriority(), task);
     }
 
     public Task take() {
@@ -25,9 +31,13 @@ public class PriorityQueue {
 
     public static void main(String[] args) {
         PriorityQueue priorityQueue = new PriorityQueue();
-        priorityQueue.put(new Task("Учить java целый день", 0));
-        priorityQueue.put(new Task("Сходить в спортзал", 3));
-        priorityQueue.put(new Task("Поспать", 7));
+        priorityQueue.put(new Task("Учить java целый день", 1));
+        priorityQueue.put(new Task("Сходить в спортзал", 2));
+        priorityQueue.put(new Task("Поспать", 3));
+        priorityQueue.put(new Task("Попить водички", 4));
+        priorityQueue.put(new Task("Поработать", 5));
+        priorityQueue.put(new Task("Сделать еще что то", 6));
+        priorityQueue.put(new Task("МУЗЫКА", 7));
         for (int i = 0; i < priorityQueue.tasks.size(); i++) {
             System.out.println(priorityQueue.tasks.get(i).getDesc());
         }
