ac4fab888ab833d4bf32e5cce514380cca69bca1
y.dubovitsky <y.dubovitsky@gmail.com>
2018-08-24 12:53:10 +0300

    Создать программу для сортировки массива методом перестановки. 1. Поправил тесты.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -5,18 +5,13 @@ package ru.job4j.array;
  * @version 1.0
  * @since 0.1
  */
-
 public class BubbleSort {
-	
-	//public static void main(String[] args) {
-	//	BubbleSort bs = new BubbleSort();
-	//	int[] mas = {9,8,-3,0,7,6,5,0,4,3,2,1};
-	//	int[] qwe = bs.sort(mas);
-	//	for(int i = 0; i < qwe.length; i++) {
-	//		System.out.print(qwe[i]);
-	//	}
-	//}		
-	
+
+	/**
+	 * Метод реализует "Пузырьковую" сортировку.
+	 * @param array - массив, который нужно отсортировать.
+	 * @return - отсортированный массив.
+	 */
 	public int[] sort(int[] array) {
 		for(int j = 0; j < array.length - 1; j++) {
 			for(int i = 0; i < array.length - 1; i++) {
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.array;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,23 +11,29 @@ import static org.junit.Assert.assertThat;
  * @version 1.0
  * @since 0.1
  */
- 
- public class BubbleSortTest {
-	 @Test
-	 public void sortTestLuck() {
-		BubbleSort bs = new BubbleSort();
-		int[] qwe = {9,8,-3,0,7,6,5,0,4,3,2,1};
-		int[] resultArray = turn.back(qwe);
-		int[] expectArray = {-300123456789};
-		assertThat(resultArray, is(expectArray));
-	}
-	
-	@Test
-	public void sortTestFail() {
-		BubbleSort bs = new BubbleSort();
-		int[] qwe = {9,8,-3,0,7,6,5,0,4,3,2,1};
-		int[] resultArray = turn.back(qwe);
-		int[] expectArray = {0-30123456789};
-		assertThat(resultArray, is(expectArray));
-	}
+
+public class BubbleSortTest {
+
+    BubbleSort bs;
+
+    @Before
+    public void initVariable() {
+        bs = new BubbleSort();
+    }
+
+    @Test
+    public void sortTestLuck() {
+        int[] qwe = {9, 8, -3, 0, 7, 6, 5, 0, 4, 3, 2, 1};
+        int[] resultArray = bs.sort(qwe);
+        int[] expectArray = {-3, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(resultArray, is(expectArray));
+    }
+
+    @Test
+    public void sortTestFail() {
+        int[] qwe = {9, 8, -3, 0, 7, 6, 5, 0, 4, 3, 2, 1};
+        int[] resultArray = bs.sort(qwe);
+        int[] expectArray = {0, -3, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(resultArray, is(expectArray));
+    }
 }
