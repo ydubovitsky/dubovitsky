74b82d24a18c7a50c44c378a597b035a4d1d6a1b
y-dubovitsky <34709153+y-dubovitsky@users.noreply.github.com>
2018-09-28 17:28:15 +0300

    Сделал откат класса ConsoleInput.

--- chapter_002/src/main/java/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/tracker/ConsoleInput.java
@@ -1,57 +1,48 @@
 package tracker;
 
 import java.util.Scanner;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 
 /**
  * Класс, отвечающий за ввод данных с клавиатуры.
  */
-public class ConsoleInput {
+public class ConsoleInput implements Input {
 
     /**
-     * Отвечает за считывание вопросов с консоли.
+     *
      */
     Scanner scanner = new Scanner(System.in);
 
     /**
-     * Функция выводит вопрос на консоль.
-     * @param question - вопрос с консоли
+     *
+     * @param question
      * @return
      */
     public String ask(String question) {
-        Consumer consumer = n -> {
-            System.out.println(n);
-        };
-        consumer.accept(question);
+        System.out.println(question);
         return scanner.nextLine();
     }
 
     /**
-     * Функция отвечает за вывод меню в диапазоне значений range.
-     * Используется лямбда-выражение и Встроенный функциональный интерфейс.
+     *
      * @param question
      * @param range
      * @return
      */
     public int ask(String question, int[] range) {
-        BiFunction<String, int[], Integer> biConsumer = (a, b) -> {
-            int key = -1;
-            do {
-                try {
-                    key = Integer.valueOf(ask(question));
-                    if (key > range.length || key < 0) {
-                        throw new MenuOutException("Необходимо выбрать значение из диапазона меню");
-                    } else {
-                        break;
-                    }
-                } catch (MenuOutException e) {
-                    System.out.println(e.getMessage());
+        int key = -1;
+        do {
+            try {
+                key = Integer.valueOf(ask(question));
+                if (key > range.length || key < 0) {
+                    throw new MenuOutException("Необходимо выбрать значение из диапазона меню");
+                } else {
+                    break;
                 }
-            } while (true);
-            return key;
-        };
-        return biConsumer.apply(question, range);
+            } catch (MenuOutException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (true);
+        return key;
     }
 }
 
