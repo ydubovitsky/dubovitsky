2a7b499be1187f31f5a58503f03daa5263965ce9
Yuri Dubovitsky <y.dubovitsky@gmail.com>
2018-11-08 11:26:02 +0300

    Junior - 5.3.0 Создать метод delete для односвязного списка.
    - добавлен метод удаления из односвязного списка.
    - написаны тесты.

---
+++ junior/chapter_001/src/main/java/list/SmpArrayList.java
@@ -0,0 +1,66 @@
+package list;
+
+import java.util.NoSuchElementException;
+
+public class SmpArrayList<e> {
+    private int size;
+    private Node<e> first;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(e date) {
+        Node<e> newLink = new Node<>(date);
+        newLink.previous = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Реализовать метод удаления первого элемент в списке.
+     */
+    public void delete() {
+        Node<e> newLink = this.first;
+        for (int i = 1; i < size; i++) {
+            newLink = newLink.previous;
+            if (i == 1) {
+                newLink.previous = null; // Обнуляем связь с первым элементом.
+                size--;
+            }
+        }
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public e get(int index) {
+        Node<e> result = this.first;
+        if (index >= size) {
+            throw new NoSuchElementException();
+        }
+        for (int i = 0; i < index; i++) {
+            result = result.previous;
+        }
+        return result.date;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<e> {
+
+        e date;
+        Node<e> previous;
+
+        Node(e date) {
+            this.date = date;
+        }
+    }
+}
