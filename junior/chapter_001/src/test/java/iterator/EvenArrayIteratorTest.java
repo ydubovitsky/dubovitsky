a74d9145b1386fd07847f2ba2179fd2ddbcb53dc
Yuri Dubovitsky <y.dubovitsky@gmail.com>
2018-11-13 09:57:55 +0300

    Junior - 5.3.1. Создать динамический список на базе массива.
    - Еще раз прогнал тесты, все проходит.

--- junior/chapter_001/src/test/java/iterator/EvenArrayIteratorTest.java
+++ junior/chapter_001/src/test/java/iterator/EvenArrayIteratorTest.java
@@ -23,7 +23,7 @@ public class EvenArrayIteratorTest {
      */
     @Before
     public void setUp() throws Exception {
-        evenArrayIterator = new EvenArrayIterator(new Integer[]{1,4,3,5,3,7,5,7,7,1,7,8});
+        evenArrayIterator = new EvenArrayIterator(new Integer[]{4,6,3});
     }
 
     /**
@@ -32,18 +32,18 @@ public class EvenArrayIteratorTest {
      */
     @Test
     public void hasNext() throws Exception {
-        assertThat(evenArrayIterator.hasNext(), is(false));
+        assertThat(evenArrayIterator.hasNext(), is(true));
         assertThat(evenArrayIterator.hasNext(), is(true));
         assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(false));
-        assertThat(evenArrayIterator.hasNext(), is(true));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(false));
+//        assertThat(evenArrayIterator.hasNext(), is(true));
     }
 
     /**
