282a894ac8e28360b03f480261ae09618e101fe0
y.dubovitsky <y.dubovitsky@gmail.com>
2018-12-18 22:57:37 +0200

    Junior - 2. Обеспечить остановку потребителя.
     - Поток producer прерывает поток consumer.

--- junior/chapter_002/src/main/java/wait/ParallelSearch.java
+++ junior/chapter_002/src/main/java/wait/ParallelSearch.java
@@ -11,44 +11,42 @@ public class ParallelSearch {
          */
         BlockQueue<Integer> queue = new BlockQueue<Integer>();
         /**
-         * Флаг, показывающий состояние потока.
+         * Два потока.
          */
-        final boolean[] flag = new boolean[1];
-        /**
-         * Поток, добавляющий элементы в очередь.
-         */
-        final Thread producer = new Thread() {
-            public void run() {
-                for (int index = 0; index != 3; index++) {
-                    queue.offer(index);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                flag[0] = true;
-            }
-        };
+        Thread producer;
+        Thread consumer;
         /**
          * Поток-потребитель; Пока производитель работает, работает и потребитель.
          */
-        Thread consumer = new Thread(() -> {
-            while (producer.isAlive() || !flag[0]) { // Пока поток производитель запущен или флаг = false;
+        consumer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) { // Пока поток производитель запущен;
                 queue.poll();
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Thread.currentThread().interrupt();
+                    System.out.println("Поток Producer прервал выполнение потока Consumer");
                 }
             }
         });
         /**
+         * Поток, добавляющий элементы в очередь.
+         */
+        producer = new Thread(() -> {
+            for (int index = 0; index != 3; index++) {
+                queue.offer(index);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            consumer.interrupt();
+        });
+        /**
          * Запускаем потоки.
          */
-        consumer.start();
         producer.start();
+        consumer.start();
     }
 
 }
