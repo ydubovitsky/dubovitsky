a7275fa36fb3159c7e5d7aa3746ab26faf14b774
y.dubovitsky <y.dubovitsky@gmail.con>
2019-02-22 16:00:14 +0300

    Junior - 1. Бот - мудрый Оракл.
    - Начал серверную сторону на Sockets.

--- junior/IO/src/main/java/socket/bot/Server.java
+++ junior/IO/src/main/java/socket/bot/Server.java
@@ -3,7 +3,6 @@ package socket.bot;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
