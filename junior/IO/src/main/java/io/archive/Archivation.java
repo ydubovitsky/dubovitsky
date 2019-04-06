391d9fc97e796849e72600da15eb9e4cdb7ee4ec
y.dubovitsky <y.dubovitsky@gmail.con>
2019-04-06 19:28:05 +0300

    Junior - 4. Архивация проекта.
    - Класс стал больее модульны.
    - Архивируется пакет с классом, из которого запускается main метод.
    
    TO DO:
    
    - Можно покрыть тестами и доработать архитектуру.

--- junior/IO/src/main/java/io/archive/Archivation.java
+++ junior/IO/src/main/java/io/archive/Archivation.java
@@ -4,28 +4,32 @@ import java.io.*;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+
 /**
- * In this class all files add in zip.
+ * In this class all files from the specified directory add into zip.
+ * to do this, use public method toZip(Dir dirname);
  */
 public class Archivation {
 
     // zip file out.
     File fileOut;
 
+    /**
+     * Enter file out name and path.
+     * @param fileOut
+     */
     public Archivation(String fileOut) {
         // needed add check
         this.fileOut = new File(fileOut);
     }
 
     /**
-     * In this method all files from path add into archive
-     * @param path
+     * In this method all files from the specified directory add into archive;
      */
-    private void getDirectories(Path path) {
+    private void archivation(Path path) {
 
         try(DirectoryStream<Path> directories = Files.newDirectoryStream(path);
             ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(fileOut))) {
@@ -53,8 +57,50 @@ public class Archivation {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        Archivation archivation = new Archivation("C:\\Users\\user\\IdeaProjects\\dubovitsky\\junior\\IO\\src\\main\\java\\io\\archive\\outfile.zip");
-        archivation.getDirectories(Paths.get("C:\\Users\\user\\IdeaProjects\\dubovitsky\\junior\\IO\\src\\main\\java\\io\\archive"));
+    /**
+     * This method toZip basis class and add in archive parent directory of project;
+     * @param dir
+     */
+    public void toZip(String dir) {
+        // create an object File;
+        File file = new File(dir);
+
+        // Get list of file
+        File[] files = file.listFiles();
+        for (File f : files) {
+            if (f.isDirectory()) {
+                this.toZip(f.getAbsolutePath());
+            } else {
+
+                // replace
+                String temp = this.replace(f.getAbsolutePath());
+                if (temp.contains(this.getClass().getCanonicalName())) {
+                    archivation(f.toPath().getParent());
+                }
+            }
+        }
+    }
+
+    /**
+     * This method replace System separator on "." in file path, or something else =)
+     * @param str - string to replace
+     * @return
+     */
+    private String replace(String str) {
+        // file system separator is used; different for various systems.
+        String fileSeparator = System.getProperty("file.separator");
+        String result = str.replace(fileSeparator, ".");
+        return result;
+    }
+
+    /**
+     * Main method
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Archivation a = new Archivation("outFile.zip");
+        a.toZip("C:\\Users\\user\\IdeaProjects\\dubovitsky\\junior\\IO\\src");
     }
 }
+
