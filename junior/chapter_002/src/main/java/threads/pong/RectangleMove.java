f7f429f06effcd8d2842bcebc556753297ef819d
y.dubovitsky <y.dubovitsky@gmail.con>
2019-05-31 13:25:37 +0300

    package threads.pong;
    Done:
    
    - Нашел нужную зависимость:
    
                <dependency>
                    <groupId>org.openjfx</groupId>
                    <artifactId>javafx-controls</artifactId>
                    <version>12.0.1</version>
                </dependency>
    
    TODO:
    
    - Нужно почитать:
    
    https://stackoverflow.com/questions/19015731/javafx-project-doesnt-compile-with-maven
    
    <dependency>
            <groupId>com.oracle</groupId>
            <artifactId>javaFX</artifactId>
            <version>2.2</version>
            <scope>system</scope>
            <systemPath>C:\Program Files\Java\jre7\lib\jfxrt.jar</systemPath>
        </dependency>

--- junior/chapter_002/src/main/java/threads/pong/RectangleMove.java
+++ junior/chapter_002/src/main/java/threads/pong/RectangleMove.java
@@ -1,41 +1,41 @@
-//package threads.pong;
-//
-//import javafx.scene.shape.Rectangle;
-//
-///**
-// * Класс, отвечающий за перемещение мячика.
-// */
-//public class RectangleMove implements Runnable {
-//    private final Rectangle rect;
-//
-//    public RectangleMove(Rectangle rect) {
-//        this.rect = rect;
-//    }
-//
-//    @Override
-//    public void run() {
-//        while (!Thread.currentThread().isInterrupted()) {
-//            while (this.rect.getX() < 300 && !Thread.currentThread().isInterrupted()) {
-//                this.move(5);
-//            }
-//            while (this.rect.getX() > 0 && !Thread.currentThread().isInterrupted()) {
-//                this.move(-5);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Метод отвечает за перемещение.
-//     *
-//     * @param offset - величина смещение.
-//     */
-//    private void move(int offset) {
-//        this.rect.setX(this.rect.getX() + offset);
-//        try {
-//            Thread.sleep(50);
-//        } catch (InterruptedException e) {
-//            System.out.println("Упс, вы нажали кнопку Выход! Нить прекращает работу!");
-//            Thread.currentThread().interrupt();
-//        }
-//    }
-//}
+package threads.pong;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ * Класс, отвечающий за перемещение мячика.
+ */
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            while (this.rect.getX() < 300 && !Thread.currentThread().isInterrupted()) {
+                this.move(5);
+            }
+            while (this.rect.getX() > 0 && !Thread.currentThread().isInterrupted()) {
+                this.move(-5);
+            }
+        }
+    }
+
+    /**
+     * Метод отвечает за перемещение.
+     *
+     * @param offset - величина смещение.
+     */
+    private void move(int offset) {
+        this.rect.setX(this.rect.getX() + offset);
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            System.out.println("Упс, вы нажали кнопку Выход! Нить прекращает работу!");
+            Thread.currentThread().interrupt();
+        }
+    }
+}
