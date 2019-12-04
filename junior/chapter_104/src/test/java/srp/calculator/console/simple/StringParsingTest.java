7e283b94567bddc94eab62ffc4267dae45573131
y.dubovitsky <y.dubovitsky@gmail.con>
2019-12-04 14:12:07 +0300

    Revert "project structure refactoring."
    This reverts commit 592da54e

---
+++ junior/chapter_104/src/test/java/srp/calculator/console/simple/StringParsingTest.java
@@ -0,0 +1,24 @@
+package srp.calculator.console.simple;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringParsingTest {
+
+    StringParsing stringParsing;
+
+    @BeforeEach
+    void setUp() {
+//        stringParsing = new StringParsing();
+    }
+
+    @Test
+    void getArg() {
+//        stringParsing.getArg("1.1+3.3");
+//        Assertions.assertEquals((Double)1.1, stringParsing.arg.get(0));
+//        Assertions.assertEquals((Double)3.3, stringParsing.arg.get(1));
+    }
+}
